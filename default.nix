95186c88422f2db1c04f82471ed28cbb4f531aab
Philip Cristiano <git@philipcristiano.com>
2025-05-22 15:32:39 -0400

    chore: Clean up nix build
    Don't run tests, include locked sqlx queries

--- default.nix
+++ default.nix
@@ -3,8 +3,19 @@ let manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 in
 pkgs.rustPlatform.buildRustPackage rec {
   pname = manifest.name;
+  nativeBuildInputs = [ pkgs.tailwindcss ];
   version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
+  cargoLock.outputHashes = {
+         "axum-embed-0.2.0" = "sha256-hcaL1NR+SNj7j1PIq2a7/2Q3EDrY57jI27yrgD2qef4=";
+         "maud-0.26.0" = "sha256-iuO26yBqnNXol0aj701zR9JZWbGEr4pQCpxqzf6gDmY=";
+       };
   src = pkgs.lib.cleanSource ./.;
+
+  # Tests require a postgres DB, don't try to do that here
+  doCheck = false;
+
+  # build environment variables
+  SQLX_OFFLINE = true;
 }
 
